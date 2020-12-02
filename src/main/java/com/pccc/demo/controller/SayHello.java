b5e1f3c1c59ad33abee1d30f9dad8f8d6d09d061
jiaqiaodan <276390152@qq.com>
2020-12-02 22:08:47 +0800

    初次提交

---
+++ src/main/java/com/pccc/demo/controller/SayHello.java
@@ -0,0 +1,14 @@
+package com.pccc.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("say")
+public class SayHello {
+
+    @RequestMapping("/hello")
+    public  String  hello(){
+        return  " hello";
+    }
+}
